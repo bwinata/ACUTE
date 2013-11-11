f1664eebc5d2b2f081881d2a217ce80d97da6ad4
Barry Winata <barry.winata@yahoo.com>
2013-11-12 01:18:10 +1100

    Started defining macros

---
+++ Makefile
@@ -0,0 +1,17 @@
+########################################################
+# Another C Unit Testing Framework (ACUTE)
+#
+# Author	:	Barry Winata
+# Date		:	11 November 2013
+# 
+# Description
+# ------------------------------------------------------
+# Makefile for generating release object files and
+# associated libraries. 
+########################################################
+
+all:
+	gcc src/main.c
+	
+clean:
+	
--- src/main.c
+++ src/main.c
@@ -1,8 +1,44 @@
-/*
- * main.c
+/**************************************************************
+ * Doubly Linked List
  *
- *  Created on: 11/11/2013
- *      Author: bwinata
- */
+ * Decription:
+ * This example provides a means to test and verify
+ * the testing framework.
+ *
+ * Author	: 	Barry Winata
+ * Date		:	11 November 2013
+ *
+ **************************************************************/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+void test_variable_args (int num, ...) {
+	va_list arguments;
+
+	double sum = 0;
+
+	va_start (arguments, num);
+
+	//printf ("--->%s\n", va_arg (arguments, char *));
+
+	va_end (arguments);
+}
+
+int main (int argc, char ** argv) {
+
+	test_variable_args (5);
+
+	printf ("Size of char = %d\n", sizeof (char));
+	printf ("Size of int = %d\n", sizeof (int));
+	printf ("Size of short = %d\n", sizeof (short));
+	printf ("Size of long = %d\n", sizeof (long));
+	printf ("Size of double = %d\n", sizeof (double));
+	printf ("Size of float = %d\n", sizeof (float));
+
+	return 0;
+}
 
 
