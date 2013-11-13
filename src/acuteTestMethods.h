25b0d7c5eacc53e50505e6563d3313b8ddcbfffe
Barry Winata <barry.winata@yahoo.com>
2013-11-14 00:33:00 +1100

    Renamed main.c for examples and updated Makefile to link in acuteAsserts.o into executables. Created new header file

---
+++ src/acuteTestMethods.h
@@ -0,0 +1,25 @@
+/**************************************************************
+ * ACUTE
+ *
+ * Decription:
+ * Test Method Defintions
+ *
+ * Author	: 	Barry Winata
+ * Date		:	14 November 2013
+ *
+ **************************************************************/
+
+#ifndef ACUTETESTMETHODS_H_
+#define ACUTETESTMETHODS_H_
+
+/*
+ * STRUCTURE DEFINITIONS
+ * ======================================================
+ */
+
+typedef struct __ACUTE_TEST_METHOD__ {
+	void (*function) (void);
+
+} ACUTE_Test_Method;
+
+#endif /* ACUTETESTMETHODS_H_ */
