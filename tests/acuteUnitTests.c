c32c2b5c3c1e0820d31ef0c3eb7d8b8cdad73aac
Barry Winata <barry.winata@yahoo.com>
2013-11-21 00:05:30 +1100

    Created unit test exe to test static functions internal to ACUTE

---
+++ tests/acuteUnitTests.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char ** argv) {
+	printf ("======================================\n");
+	printf ("ACUTE Unit Tests\n");
+	printf ("======================================\n");
+
+	printf ("Unit Test: ACUTE_unitTest__insertTestMethodIntoQueue\n");
+	ACUTE_unitTest__insertTestMethodIntoQueue ();
+
+	return 0;
+}
